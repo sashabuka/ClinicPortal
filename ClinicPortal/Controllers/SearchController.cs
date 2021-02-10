bf008dc3db17592d6bb8ac8c770f44db077fef9b
Alexander Burakovski <alexander.burakovski@mobilesolutions.net>
2021-02-11 01:23:08 +0300

    Added base functionality

---
+++ ClinicPortal/Controllers/SearchController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ClinicPortal.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(SearchModel.Empty);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index([FromForm] SearchModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View(SearchModel.Empty);
+                model.SearchString = string.Empty;
+                model.Result = new List<SearchResultModel>()
+                {
+                    new SearchResultModel() {Name = "Test1"},
+                    new SearchResultModel() {Name = "Test2"}
+                };
+
+                return View(model);
+            }
+            catch
+            {
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+
+    public class SearchResultModel
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+
+    public class SearchModel
+    {
+        [Required(ErrorMessage = "Please enter search term")]
+        public string SearchString { get; set; }
+
+        public IEnumerable<SearchResultModel> Result { get; set; }
+
+        [JsonIgnore] public static SearchModel Empty => new SearchModel();
+    }
+}
